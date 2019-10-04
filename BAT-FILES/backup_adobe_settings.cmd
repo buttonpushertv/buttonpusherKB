7dd30df54180d29c107c2bd32f349658702acedc
Ben Howard <bhoward@postopmedia.com>
2019-10-04 17:49:16 -0400

    Create backup_adobe_settings.cmd

---
+++ BAT-FILES/backup_adobe_settings.cmd
@@ -0,0 +1,3 @@
+for /f "delims=" %%a in (C:\BPTV-KB\PERSONAL\location.txt) do set location=%%a
+
+copy "C:\Users\ben\OneDrive\Documents\Adobe\Premiere Pro\13.0\Profile-ben\Win\BEN-CC19.kys" "C:\BPTV-KB\PERSONAL\ADOBE-SETTINGS"
