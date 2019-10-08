33032e659f35f60e36117fc6f118027ecbaaa48e
Ben Howard <ben@buttonpusher.tv>
2019-10-07 21:34:37 -0400

    Update 0_daily.cmd

--- BAT-FILES/SCHEDULED-BATS/0_daily.cmd
+++ BAT-FILES/SCHEDULED-BATS/0_daily.cmd
@@ -5,10 +5,10 @@
 
 @echo location : %1
 
-copy C:\BPTV-KB\settings.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS
-copy C:\BPTV-KB\UTIL-APPS\vnchelper\vnchelper.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS
-copy C:\BPTV-KB\UTIL-APPS\QAP\QuickAccessPopup.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS
-copy "C:\Users\ben\OneDrive\Documents\Adobe\Premiere Pro\13.0\Profile-ben\Win\BEN-CC19.kys" C:\BPTV-KB\PRIVATE\ADOBE-SETTINGS
+copy C:\BPTV-KB\settings.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS\
+copy C:\BPTV-KB\UTIL-APPS\vnchelper\vnchelper.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS\
+copy C:\BPTV-KB\UTIL-APPS\QAP\QuickAccessPopup.ini C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS\
+copy "C:\Users\ben\OneDrive\Documents\Adobe\Premiere Pro\13.0\Profile-ben\Win\BEN-CC19.kys" C:\BPTV-KB\PRIVATE\SETTINGS-BACKUPS\ADOBE-SETTINGS\
 
 for /f "delims=" %%a in (C:\BPTV-KB\PRIVATE\backup_destination.txt) do set backupdestroot=%%a
 set backupdest=%backupdestroot%PRIVATE-%1
