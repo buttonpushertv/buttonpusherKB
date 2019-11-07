9b92e2f2248da27f9d88c9d575f9a2da8499430a
Ben Howard <bhoward@postopmedia.com>
2019-11-07 17:17:10 -0500

    Update vnchelper.cmd

--- BAT-FILES/STREAMDECK-BATS/vnchelper.cmd
+++ BAT-FILES/STREAMDECK-BATS/vnchelper.cmd
@@ -1,5 +1,5 @@
 REM This will use initool.exe to read the location of the rootFolder from settings.ini
-@FOR /F "tokens=* USEBACKQ" %%F IN (`..\UTIL-APPS\initool\initool.exe g ..\settings.ini Settings rootFolder --value-only`) DO (
+@FOR /F "tokens=* USEBACKQ" %%F IN (`..\..\UTIL-APPS\initool\initool.exe g ..\..\settings.ini Settings rootFolder --value-only`) DO (
 SET rootFolder=%%F
 )
 
