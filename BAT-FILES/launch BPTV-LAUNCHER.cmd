c4b5af612c06a54804e0a822a5da1a7ea9aee3c0
buttonpushertv <ben@buttonpusher.tv>
2020-04-27 10:25:38 -0400

    fixing issues with Backups not running on multiple systems
    I noticed that backups were not running on my laptop. This is the first round of attempts to make it work more flexibly.

--- BAT-FILES/launch BPTV-LAUNCHER.cmd
+++ BAT-FILES/launch BPTV-LAUNCHER.cmd
@@ -1,6 +1,14 @@
-REM This will use initool.exe to read the location of the rootFolder from settings.ini
-@FOR /F "tokens=* USEBACKQ" %%F IN (`..\UTIL-APPS\initool\initool.exe g ..\settings.ini Settings rootFolder --value-only`) DO (
+@echo off
+@REM IMPORTANT-This batch file requires an arguement be passed that is the root folder where your install of BPTV-KB is running from. It is probably best if you just leave it as 'C:\BPTV-KB".
+
+@echo The current directory is %CD%
+@echo and this is what got passed: %1
+
+@REM This will use initool.exe to read the location of the rootFolder from settings.ini
+@FOR /F "tokens=* USEBACKQ" %%F IN (`%1\UTIL-APPS\initool\initool.exe g %1\settings.ini Settings rootFolder --value-only`) DO (
 SET rootFolder=%%F
 )
-cd %rootFolder%
+@cd %rootFolder%
+@echo Now the current directory is %CD%
+
 BPTV-LAUNCHER.ahk
