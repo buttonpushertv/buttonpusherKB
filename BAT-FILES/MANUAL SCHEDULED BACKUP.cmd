20352ab27e8b3d328321ed63b6e1f0ee3a34c227
Ben Howard <bhoward@postopmedia.com>
2020-04-29 19:15:56 -0400

    fixes to manual scheduled backup BAT file
    There was an issue with this manual method for forcing a backup to run. I think it is resolved now.

--- BAT-FILES/MANUAL SCHEDULED BACKUP.cmd
+++ BAT-FILES/MANUAL SCHEDULED BACKUP.cmd
@@ -1,12 +1,18 @@
-@echo off
+@echo on
+@REM IMPORTANT-This batch file requires an arguement be passed that is the root folder where your install of BPTV-KB is running from. It is probably best if you just leave it as 'C:\BPTV-KB".
 
-REM This will use initool.exe to read the location of the rootFolder from settings.ini
-FOR /F "tokens=* USEBACKQ" %%F IN (`..\UTIL-APPS\initool\initool.exe g ..\settings.ini Settings rootFolder --value-only`) DO (
-SET rootFolder=%%F
-)
 
-@REM this command should redirect output of this batch to a log file
-> %rootFolder%\BAT-FILES\LOGS\MANUAL_last_run_log.txt (
+@echo The current directory is %CD%
+@echo and this is what got passed: %1
+
+SET rootFolder=%1
+
+@echo Rootfolder is: %rootFolder%
+
+IF EXIST %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt del %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt
+
+REM this command should redirect output of this batch to a log file
+> %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt (
 
 @echo =================================
 @echo MANUALLY RUNNING SCHEDULED BACKUP
@@ -14,22 +20,21 @@ SET rootFolder=%%F
 
 @echo Rootfolder is: %rootFolder%
 
-@echo Deleting %rootFolder%\PRIVATE\EDIT2-PC\last_daily_run.txt
+@echo Deleting %rootFolder%\PRIVATE\%computername%\last_daily_run.txt
 @echo This should allow the backup to be run even if it had already run today.
-del %rootFolder%\PRIVATE\EDIT2-PC\last_daily_run.txt
+IF EXIST %rootFolder%\PRIVATE\%computername%\last_daily_run.txt del %rootFolder%\PRIVATE\%computername%\last_daily_run.txt
 
 @echo And this will set the date of the %rootFolder%\PRIVATE\%computername%\days_since_system_backup.txt to yesterday
 powershell -command "$(Get-Item %rootFolder%\PRIVATE\%computername%\days_since_system_backup.txt).LastWriteTime=(Get-Date).AddHours(-24)"
 
-
-@REM Set working directory to the location of the SCHEDULED-BATS
-@CD %rootFolder%\BAT-FILES\SCHEDULED-BATS
+REM Set working directory to the location of the SCHEDULED-BATS
+CD %rootFolder%\BAT-FILES\SCHEDULED-BATS
 
 @echo ++ CALLING scheduled-bats.cmd ++
-@call %rootFolder%\BAT-FILES\SCHEDULED-BATS\scheduled-bats.cmd
+call %rootFolder%\BAT-FILES\SCHEDULED-BATS\scheduled-bats.cmd %rootFolder%
 @echo ++ DONE CALLING scheduled-bats.cmd ++
 
-@cd ..
+cd ..
 
 @echo ---------------------------------------
 @echo MANUAL RUN OF SCHEDLUED BACKUP COMPLETE
