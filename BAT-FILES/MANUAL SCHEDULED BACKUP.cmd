81630a694154828f1ecdbb9de9ded7e57b08b40c
Ben Howard <ben@buttonpusher.tv>
2020-06-22 22:45:48 -0400

    Update MANUAL SCHEDULED BACKUP.cmd

--- BAT-FILES/MANUAL SCHEDULED BACKUP.cmd
+++ BAT-FILES/MANUAL SCHEDULED BACKUP.cmd
@@ -1,11 +1,15 @@
 @echo on
-@REM IMPORTANT-This batch file requires an arguement be passed that is the root folder where your install of buttonpusherKB is running from. It is probably best if you just leave it as 'C:\BKB".
-
+@REM This BAT file will read a Environment Variable of BKB_ROOT to get the stored value of the root folder for buttonpusherKB's scripts and files
 SET rootFolder=%BKB_ROOT%
 
 @echo Rootfolder is: %rootFolder%
 
-IF EXIST %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt del %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt
+@REM The line below is mostly for new or first times running this BAT file. If the commands below this can't find the paths to the files they are looking for it will fail.
+@IF NOT EXIST %rootFolder%\PRIVATE\%computername% mkdir %rootFolder%\PRIVATE\%computername%
+@IF NOT EXIST %rootFolder%\PRIVATE\%computername%\LOGS\ mkdir %rootFolder%\PRIVATE\%computername%\LOGS
+
+@REM This command will delete the file manual_last_run_log.txt
+@IF EXIST %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt del %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt
 
 REM this command should redirect output of this batch to a log file
 > %rootFolder%\PRIVATE\%computername%\LOGS\manual_last_run_log.txt (
@@ -15,12 +19,12 @@ REM this command should redirect output of this batch to a log file
 @echo =================================
 
 @echo Rootfolder is: %rootFolder%
-
+@REM This command will delete the last_daily_run.txt file so that it should force all the backup actions to occur
 @echo Deleting %rootFolder%\PRIVATE\%computername%\last_daily_run.txt
 @echo This should allow the backup to be run even if it had already run today.
 IF EXIST %rootFolder%\PRIVATE\%computername%\last_daily_run.txt del %rootFolder%\PRIVATE\%computername%\last_daily_run.txt
 
-@echo And this will set the date of the %rootFolder%\PRIVATE\%computername%\days_since_system_backup.txt to yesterday
+@REM And this will set the date of the %rootFolder%\PRIVATE\%computername%\days_since_system_backup.txt to yesterday
 powershell -command "$(Get-Item %rootFolder%\PRIVATE\%computername%\days_since_system_backup.txt).LastWriteTime=(Get-Date).AddHours(-24)"
 
 REM Set working directory to the location of the SCHEDULED-BATS
