807e83161b36381c1153e24acf7c204ad092f9cf
Ben Howard <bhoward@postopmedia.com>
2020-10-13 13:43:33 -0400

    Update ffprores.cmd
    Added the ability to just put in the source filename and it will change the filename to have a '.mov' extension & append '-PR422' to the filename.

--- BAT-FILES/ffmpeg_bats/ffprores.cmd
+++ BAT-FILES/ffmpeg_bats/ffprores.cmd
@@ -1,4 +1,5 @@
 REM this batch file will convert the input file to ProRes
 REM you need to supply both the input filename and the output file name
-
-ffmpeg -i %1 -c:v prores -profile:v 3 %2
+set fname=%~n1
+set newname="%fname%-PR422.mov"
+ffmpeg -i %1 -c:v prores -profile:v 3 %newname%
