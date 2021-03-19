e152566072fbb7852ea4efacba644f210883be97
Ben Howard <ben@buttonpusher.tv>
2021-03-19 17:03:04 -0400

    fixing clearall.cmd
    to remove all saved project locations for DRAKE

--- SCRIPTS-UTIL/STREAMDECK/DRAKE/clearall.cmd
+++ SCRIPTS-UTIL/STREAMDECK/DRAKE/clearall.cmd
@@ -3,6 +3,7 @@ set rootFolder=%BKB_ROOT%
 del %rootFolder%\PRIVATE\%computername%\Project-1.txt
 del %rootFolder%\PRIVATE\%computername%\Project-2.txt
 del %rootFolder%\PRIVATE\%computername%\Project-3.txt
+del %rootFolder%\PRIVATE\%computername%\Project-4.txt
 del %rootFolder%\PRIVATE\%computername%\Project-TEMP.txt
 
 
