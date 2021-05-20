1e8bd899d23a83a679db9a3a944aceef7ca1ed55
Ben Howard <bhoward@postopmedia.com>
2021-05-19 20:38:46 -0400

    Update ffprores.cmd

--- BAT-FILES/ffmpeg_bats/ffprores.cmd
+++ BAT-FILES/ffmpeg_bats/ffprores.cmd
@@ -2,4 +2,11 @@ REM this batch file will convert the input file to ProRes
 REM you need to supply both the input filename and the output file name
 set fname=%~n1
 set newname="%fname%-PR422.mov"
+
+if not exist "originals\" (
+    mkdir originals
+)
+
 ffmpeg -i %1 -c:v prores -profile:v 3 %newname%
+
+move %1 originals\
